225e6c8b3e68b3771235d89a908c48595c6c8ec2
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-08-30 10:43:21 +0700

    Feature / Update style for scrollbar and handle drag when list is empty

--- components/sections/project-column.tsx
+++ components/sections/project-column.tsx
@@ -1,9 +1,13 @@
 'use client'
 
 import React from 'react'
-import { Draggable } from '@hello-pangea/dnd'
+import { Draggable, Droppable } from '@hello-pangea/dnd'
+
+// Components
 import { CardsContainer } from './cards-container'
 import { ProjectColumnContent } from './project-column-content'
+import { Button } from '../ui/button'
+import { PlusIcon } from '@/icons/plus-icon'
 import { ProjectDetail } from '@/lib/models'
 
 export interface ProjectColumnProps {
@@ -16,17 +20,33 @@ export interface ProjectColumnProps {
 export const ProjectColumn = ({ title, projects, isDragDisabled = false, index }: ProjectColumnProps) => {
   return (
     <Draggable isDragDisabled={isDragDisabled} draggableId={title} index={index}>
-      {(provided, snapshot) => (
-        <CardsContainer
-          innerRef={provided.innerRef}
-          title={isDragDisabled ? '' : title}
-          draggable={snapshot.isDragging}
-          className='w-[302px] h-fit'
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          {projects && <ProjectColumnContent listId={title} listType='PROJECT' projects={projects} />}
-        </CardsContainer>
+      {(dragProvided, snapshot) => (
+        <Droppable droppableId={title} type='PROJECT'>
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              className='flex flex-col'
+              {...dragProvided.draggableProps}
+              {...dragProvided.dragHandleProps}
+            >
+              <CardsContainer
+                innerRef={dragProvided.innerRef}
+                title={isDragDisabled ? '' : title}
+                draggable={snapshot.isDragging}
+                className='w-[302px] h-fit max-h-[80dvh] overflow-hidden rounded-b-none pb-5'
+              >
+                {projects && <ProjectColumnContent listId={title} listType='PROJECT' projects={projects} />}
+              </CardsContainer>
+              <Button
+                variant='secondary'
+                className='rounded-none rounded-b-3xl bg-card shadow-sm border border-t-0 border-separator'
+              >
+                <PlusIcon />
+              </Button>
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
       )}
     </Draggable>
   )
