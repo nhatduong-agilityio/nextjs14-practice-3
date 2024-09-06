d5802ac78f14bc660e0757c169ccc36bbb7989c1
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2024-09-06 17:47:57 +0700

    Feature / Implement handle logic for sign-in page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,54 +1,9 @@
-'use client'
-
 import * as React from 'react'
-import { useSignIn } from '@clerk/nextjs'
-import { useRouter } from 'next/navigation'
-import { Input } from '@/components/ui/input'
-import { Heading } from '@/components/ui/heading'
-import { EmailIcon } from '@/icons/email-icon'
 
 // Features
 import { SignInForm } from '@/features/auth/components/sign-in-form'
 
 const SignInPage = () => {
-  const { isLoaded, signIn, setActive } = useSignIn()
-  const [email, setEmail] = React.useState('')
-  const [password, setPassword] = React.useState('')
-  const router = useRouter()
-
-  // Handle the submission of the sign-in form
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-
-    if (!isLoaded) {
-      return
-    }
-
-    // Start the sign-in process using the email and password provided
-    try {
-      const signInAttempt = await signIn.create({
-        identifier: email,
-        password,
-      })
-
-      // If sign-in process is complete, set the created session as active
-      // and redirect the user
-      if (signInAttempt.status === 'complete') {
-        await setActive({ session: signInAttempt.createdSessionId })
-        router.push('/')
-      } else {
-        // If the status is not complete, check why. User may need to
-        // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2))
-      }
-    } catch (err: any) {
-      // See https://clerk.com/docs/custom-flows/error-handling
-      // for more info on error handling
-      console.error(JSON.stringify(err, null, 2))
-    }
-  }
-
-  // Display a form to capture the user's email and password
   return (
     <div className='flex flex-col sm:min-w-[506px] pt-2.5 px-2.5 overflow-y-scroll no-scrollbar'>
       <SignInForm />
--- app/(home)/(dashboard)/page.tsx
+++ app/(home)/(dashboard)/page.tsx
@@ -1,13 +1,3 @@
-'use client'
-
-import Image from 'next/image'
-import { useTheme } from 'next-themes'
-import { SignOutButton } from '@clerk/nextjs'
-
-// Components
-import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 import { PageHeader } from '@/components/sections/page-header'
 import { Text } from '@/components/ui/text'
 
@@ -16,25 +6,6 @@ import { TeamListCard } from '@/features/teams/components/team-list-card'
 import { ProjectListCard } from '@/features/projects/components/project-list-card'
 
 const Dashboard = () => {
-  const { setTheme } = useTheme()
-
-  const renderToggleTheme = () => (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
-          <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-          <span className='sr-only'>Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-
   return (
     <div className='flex h-full flex-col p-5 lg:p-10'>
       <PageHeader className='gap-2.5'>
@@ -42,7 +13,6 @@ const Dashboard = () => {
           here’s your currently projects
         </Text>
       </PageHeader>
-
       <div className='w-full flex flex-col gap-5'>
         <TeamListCard />
         <ProjectListCard />
